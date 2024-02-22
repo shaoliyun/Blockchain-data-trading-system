b49ace15fd234c30508d06cc04d0f28d7761751e
shaoliyun <101045712+shaoliyun@users.noreply.github.com>
2024-02-22 18:21:49 +0800

    Add files via upload

--- contracts/UserRegistry.sol
+++ contracts/UserRegistry.sol
@@ -1,5 +1,3 @@
-pragma solidity ^0.8.0;
-
 contract UserRegistry {
     struct User {
         string name;
@@ -7,18 +5,15 @@ contract UserRegistry {
         uint256 phone;
         uint256 creditValue;
         string[] characterTypes;
-        bytes32 passwordHash;
     }
 
-    mapping(uint256 => User) private users;
-    mapping(uint256 => bool) public validUserId;
+    mapping(address => User) private users;
+    mapping(address => bool) public validUserAddress;
     mapping(string => bool) public validCharacterTypes;
 
-    uint256 private nextUserId = 1;
-
-    event UserRegistered(uint256 userId, string name, string email, uint256 phone, uint256 creditValue, string[] characterTypes);
-    event UserUnregistered(uint256 userId);
-    event UserUpdated(uint256 userId, string name, string email, uint256 phone, uint256 creditValue, string[] characterTypes);
+    event UserRegistered(address userAddress, string name, string email, uint256 phone, uint256 creditValue, string[] characterTypes);
+    event UserUnregistered(address userAddress);
+    event UserUpdated(address userAddress, string name, string email, uint256 phone, uint256 creditValue, string[] characterTypes);
 
     constructor(string[] memory _validCharacterTypes) {
         for (uint256 i = 0; i < _validCharacterTypes.length; i++) {
@@ -26,7 +21,7 @@ contract UserRegistry {
         }
     }
 
-    function registerUser(string memory name, string memory email, uint256 phone, uint256 creditValue, string[] memory characterTypes, bytes32 passwordHash) public {
+    function registerUser(string memory name, string memory email, uint256 phone, uint256 creditValue, string[] memory characterTypes, bytes memory signature) public {
         require(bytes(name).length > 0, "Name is required");
         require(bytes(email).length > 0, "Email is required");
         require(phone > 0, "Phone number is required");
@@ -34,30 +29,38 @@ contract UserRegistry {
         require(characterTypes.length > 0, "At least one character type is required");
         require(validateCharacterTypes(characterTypes), "Invalid character types");
 
-        users[nextUserId] = User(name, email, phone, creditValue, characterTypes, passwordHash);
+        bytes32 message = prefixed(keccak256(abi.encode(msg.sender, name, email, phone, creditValue, characterTypes)));
 
-        emit UserRegistered(nextUserId, name, email, phone, creditValue, characterTypes);
+        require(recoverSigner(message, signature) == msg.sender, "Invalid signature");
 
-        validUserId[nextUserId] = true;
-        nextUserId++;
+        users[msg.sender] = User(name, email, phone, creditValue, characterTypes);
+
+        emit UserRegistered(msg.sender, name, email, phone, creditValue, characterTypes);
+
+        validUserAddress[msg.sender] = true;
     }
 
-    function unregisterUser(uint256 userId, bytes32 passwordHash) public {
-        require(validUserId[userId], "User does not exist");
-        require(validatePassword(userId, passwordHash), "Invalid password");
+    function unregisterUser(bytes memory signature) public {
+        require(validUserAddress[msg.sender], "User does not exist");
 
-        delete users[userId];
-        validUserId[userId] = false;
+        bytes32 message = prefixed(keccak256(abi.encode(msg.sender)));
+        require(recoverSigner(message, signature) == msg.sender, "Invalid signature");
 
-        emit UserUnregistered(userId);
+        delete users[msg.sender];
+        validUserAddress[msg.sender] = false;
+
+        emit UserUnregistered(msg.sender);
     }
 
-    function updateUser(uint256 userId, bytes32 passwordHash, string memory name, string memory email, uint256 phone, uint256 creditValue, string[] memory characterTypes) public {
-        require(validUserId[userId], "User does not exist");
-        require(validatePassword(userId, passwordHash), "Invalid password");
+    function updateUser(bytes memory signature, string memory name, string memory email, uint256 phone, uint256 creditValue, string[] memory characterTypes) public {
+        require(validUserAddress[msg.sender], "User does not exist");
         require(validateCharacterTypes(characterTypes), "Invalid character types");
 
-        User storage user = users[userId];
+        bytes32 message = prefixed(keccak256(abi.encode(msg.sender, name, email, phone, creditValue, characterTypes)));
+
+        require(recoverSigner(message, signature) == msg.sender, "Invalid signature");
+
+        User storage user = users[msg.sender];
         if (bytes(name).length > 0) {
             user.name = name;
         }
@@ -74,11 +77,7 @@ contract UserRegistry {
             user.characterTypes = characterTypes;
         }
 
-        emit UserUpdated(userId, user.name, user.email, user.phone, user.creditValue, user.characterTypes);
-    }
-
-    function validatePassword(uint256 userId, bytes32 passwordHash) private view returns (bool) {
-        return users[userId].passwordHash == passwordHash;
+        emit UserUpdated(msg.sender, user.name, user.email, user.phone, user.creditValue, user.characterTypes);
     }
 
     function validateCharacterTypes(string[] memory characterTypes) private view returns (bool) {
@@ -90,12 +89,48 @@ contract UserRegistry {
         return true;
     }
 
-    function getUser(uint256 userId, bytes32 passwordHash) public view returns (User memory) {
-        require(validUserId[userId], "User does not exist");
-        require(validatePassword(userId, passwordHash), "Invalid password");
+    //获取用户信息，用于信息重置时查询原信息，或查询信誉度、角色
+    function getUser(address userAddress, bytes memory signature, bool secretMask) public view returns (User memory) {
+        require(validUserAddress[userAddress], "User does not exist");
+        require(validUserAddress[msg.sender], "asker does not exist");
 
-        User memory user = users[userId];
-        delete user.passwordHash;
+        bytes32 message = prefixed(keccak256(abi.encode(msg.sender)));
+        require(recoverSigner(message, signature) == msg.sender, "Invalid signature");
+
+        User memory user = users[userAddress];
+
+        if(secretMask){
+            user.email = "";
+            user.phone = 999999999;
+        }
         return user;
+    }
+
+    // Taken from https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/cryptography/ECDSA.sol
+    function recoverSigner(bytes32 message, bytes memory sig) internal pure returns (address) {
+        require(sig.length == 65, "Invalid signature length");
+
+        bytes32 r;
+        bytes32 s;
+        uint8 v;
+
+        assembly {
+            r := mload(add(sig, 32))
+            s := mload(add(sig, 64))
+            v := byte(0, mload(add(sig, 96)))
+        }
+
+        if (v < 27) {
+            v += 27;
+        }
+
+        require(v == 27 || v == 28, "Invalid signature");
+
+        return ecrecover(message, v, r, s);
+    }
+
+    // Taken from https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/cryptography/ECDSA.sol
+    function prefixed(bytes32 hash) internal pure returns (bytes32) {
+        return keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", hash));
     }
 }
